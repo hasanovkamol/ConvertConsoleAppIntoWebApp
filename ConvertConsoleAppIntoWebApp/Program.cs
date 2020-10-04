4ce4246bb08d7203894839c4a50679f29fd37dc9
hasanovkamol <71973508+hasanovkamol@users.noreply.github.com>
2020-10-04 18:01:22 +0500

    Add project files.

---
+++ ConvertConsoleAppIntoWebApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace ConvertConsoleAppIntoWebApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CrearteHostBulider(args).Build().Run();
+        }
+        public static IHostBuilder CrearteHostBulider(string[] arge) =>
+            Host.CreateDefaultBuilder(arge).ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+        
+    }
+}
---
+++ ConvertConsoleAppIntoWebApp/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace ConvertConsoleAppIntoWebApp
+{
+   public class Startup
+    {
+        public void ConfigurationServices(IServiceCollection services)
+        {
+
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.Use(async (context, next) => 
+            {
+                await context.Response.WriteAsync("\nHello this second ");
+                await next();
+                await context.Response.WriteAsync("\nHusanov Doston");
+            }
+            );
+            app.Use(async (context, next) => 
+            {
+                await context.Response.WriteAsync("\nKamol Hasanov");
+            }
+            );
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                 {
+                     await context.Response.WriteAsync("Hello! This project Convert Console Application into Web Application");
+
+                 }
+                );
+            });
+        }
+    }
+}
